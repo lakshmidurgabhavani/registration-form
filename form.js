e424ccfcd1c1831cef1ad40f777c213f5a114b3b
lakshmidurgabhavani <123814686+lakshmidurgabhavani@users.noreply.github.com>
2023-02-23 23:31:49 +0530

    Add files via upload

---
+++ form.js
@@ -0,0 +1,10 @@
+function showInput() {
+    document.getElementById('display_user_name').innerHTML =
+        document.getElementById("user_name").value;
+    document.getElementById('display_user_email').innerHTML =
+        document.getElementById("user_email").value;
+    document.getElementById('display_user_mobileNo').innerHTML =
+        document.getElementById("user_mobileNo").value;
+    document.getElementById('display_user_address').innerHTML =
+        document.getElementById("user_address").value;
+}
